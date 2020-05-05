4d042118bf451bdc6e612d62962666e4b3f01b9b
ffmsdazuma <azuma.takahashi12@gmail.com>
2020-05-05 18:59:22 +0900

    implement take photo funcion

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -8,7 +8,7 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>釣果予測</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
